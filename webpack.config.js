229cf69bd75d637833849a1db528628da08ff5c6
relogn <relogn@mail.com>
2017-03-01 14:00:34 +0200

    Babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,49 @@
+const path = require('path');
+const webpack = require('webpack');
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+
 module.exports = {
   entry: './src/index',
   output: {
-    filename: 'dist/react-data-components.min.js',
+    path: path.join(__dirname, 'dist'),
+    filename: 'react-editable-table.min.js',
     library: 'ReactDataComponents',
-    libraryTarget: 'umd'
+    libraryTarget: 'umd',
   },
   externals: {
     'react': {
       root: 'React',
       amd: 'react',
       commonjs: 'react',
-      commonjs2: 'react'
+      commonjs2: 'react',
     },
-    'react/addons': {
-      root: 'React',
-      amd: 'react',
-      commonjs: 'react',
-      commonjs2: 'react'
-    }
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'jsx?harmony' }
-    ]
-  }
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        /* query: {
+          plugins: ['lodash'],
+        }, */
+      },
+    ],
+  },
+  plugins: [
+    new LodashModuleReplacementPlugin({
+      shorthands: true,
+      collections: true,
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        screw_ie8: true,
+        warnings: false,
+      },
+    }),
+  ],
 };
